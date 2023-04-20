ce6ea331b25ee0fc6b51eddd9b7be5a3cbe2ed97
ewertontoledo <ewerton.silva@sysmap.com.br>
2023-04-20 14:22:23 -0300

    update  deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
    const response = {
        statusCode: 200,
-       body: JSON.stringify('Deploy Prototipo API 2'),
+       body: JSON.stringify('Deploy API teste 3'),
    };
    return response;
 };
